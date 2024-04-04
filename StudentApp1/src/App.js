54c818761a2602f1907a5ca5c88f810e8d9a68c1
Panicshoe <86228058+Panicshoe@users.noreply.github.com>
2024-04-03 23:14:33 -0400

    Initial commit

---
+++ StudentApp1/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import StudentForm from './Components/StudentForm'; 
+
+const App = () => {
+  return (
+    <div>
+      <h1>StudentApp1</h1>
+      <StudentForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ StudentApp3/Src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { StudentForm } from '../../StudentApp1/src/Components/StudentForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>StudentApp3</h1>
+      <StudentForm />
+    </div>
+  );
+};
+
+export default App;
